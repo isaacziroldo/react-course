f80ddb696b144638d8cf7140b597e3045849a8fd
isaacziroldo <50035007+isaacziroldo@users.noreply.github.com>
2021-05-30 21:15:16 -0300

    first commit

---
+++ fundamentos-react/src/components/basics/first.js
@@ -0,0 +1,4 @@
+
+export default function First(){
+    return 'First Component'
+}
---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+import React from 'react'
+import ReactDom from 'react-dom'
+
+import _First from './components/basics/first'
+
+
+ReactDom.render(
+    <div>
+        <_First>
+            
+        </_First>
+    </div>,
+    document.getElementById('root')
+)
